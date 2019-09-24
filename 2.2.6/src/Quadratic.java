7d7ad3a4aef0f8036100cc9d6dac1b4a51decaaf
Kosmina_Natallia <natallia.kasmina@instinctools.com>
2019-09-24 19:15:42 +0300

    task 2.2.6

---
+++ 2.2.6/src/Quadratic.java
@@ -0,0 +1,31 @@
+public class Quadratic {
+
+    public static void main(String[] args) {
+        //a*x^2+b*x+c=0
+        //D=b^2-4*a*c
+        //x1=(-b-sqrt(D))/2*a
+        //x2=(-b+sqrt(D))/2*a
+        //1 Case: D > 0
+        double D;
+        double x1;
+        double x2;
+        double b = Double.parseDouble(args[1]);
+        double a = Double.parseDouble(args[0]);
+        double c = Double.parseDouble(args[2]);
+
+
+        D = Math.pow(b, 2)-4*a*c;
+        System.out.println("D = "+" "+D);
+        if (D > 0) {
+            x1 = (-b+Math.sqrt(D))/(2*a);
+            x2 = (-b-Math.sqrt(D))/(2*a);
+            System.out.println("x1 = "+" "+x1);
+            System.out.println("x2 = "+" "+x2);
+        } else if (D == 0) {
+            x1 = x2 = -b/(2*a);
+            System.out.println("x1 = x2 "+" "+x1);
+        } else if (D < 0) {
+            System.out.println("The roots of the equation do not exist");
+        }
+    }
+}

3c5a4aac15975b762f64f9db2248a794b93cf0db
Kosmina_Natallia <natallia.kasmina@instinctools.com>
2019-09-25 21:33:50 +0300

    any edits

--- 2.2.8/src/RandomNumber/NumberRandom.java
+++ 2.2.8/src/RandomNumber/NumberRandom.java
@@ -2,8 +2,16 @@ package RandomNumber;
 
 public class NumberRandom {
     public static void main(String[] args) {
+        if (args[0].length() > 10) {
+            System.out.println("Number is too long. Please try another one.");
+            System.exit(-1);
+        }
         try {
             int number = Integer.parseInt(args[0]);
+            if (number<0){
+                System.out.println("Incorrect value. Please enter a positive number.");
+                System.exit(-1);
+            }
             System.out.println(generatedNumber(number));
         } catch (Exception e) {
             System.out.println("Please enter number!");

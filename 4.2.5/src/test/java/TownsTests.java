10f0f6784f6a5c4e2c911f184d39a95dcac5ebf8
Kosmina_Natallia <natallia.kasmina@instinctools.com>
2019-10-13 19:28:07 +0300

    unit test for one method

--- 4.2.5/src/test/java/TownsTests.java
+++ 4.2.5/src/test/java/TownsTests.java
@@ -1,7 +1,14 @@
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class TownsTests {
-/*    @Test
-    public void
-    */
+    Towns towns = new Towns();
+    @Test
+    public void compareTest() {
+        String firstWord = "Борисов";
+        String secondWord = "Обь";
+        char expected = 'б';
+        assertEquals(expected, towns.compareSymbols(firstWord, secondWord));
+    }
 }
